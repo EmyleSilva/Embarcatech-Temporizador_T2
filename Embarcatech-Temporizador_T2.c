4085ba409b9b039e6e15793de25570f7f9b9a436
EmyleSilva <emylesnt.silva@gmail.com>
2025-02-03 18:52:11 -0300

    Documentação do código

--- Embarcatech-Temporizador_T2.c
+++ Embarcatech-Temporizador_T2.c
@@ -18,7 +18,6 @@
 /**
  * Variáveis globais
  */
-
 int counter = 0;
 bool leds_active = false;
 const uint8_t pins[] = {led_g, led_b, led_r}; 
@@ -43,28 +42,43 @@ void init_leds()
     gpio_init(led_b);
     gpio_set_dir(led_b, GPIO_OUT);
 }
-
+/**
+ * Função para inicializar o botão
+ */
 void init_button()
 {
     gpio_init(BUTTON_A);
     gpio_set_dir(BUTTON_A, GPIO_IN);
     gpio_pull_up(BUTTON_A);
 }
-
+/**
+ * Altera os estados dos LEDS
+ */
 void turn_on_leds()
 {
     for (int i = 0; i < 3; i++)
         gpio_put(pins[i], leds_states[counter][i]);
 }
 
+/**
+ * Função de callback para mudar os estados dos LEDS
+ */
 int64_t turn_off_callback(alarm_id_t id, void *user_data)
 {
     if (counter < 3)
     {
         turn_on_leds();
         counter++;
+        /**
+         * Chama a função 'turn_off_callback' a cada 3 segundos,
+         * enquanto ainda há leds acessos
+         */
         add_alarm_in_ms(3000, turn_off_callback, NULL, false);
     }else {
+        /**
+         * Quando todos os leds são apagados, atualiza as variaveis para que
+         * o botão possa ser acionado novamente
+         */
         turn_on_leds();
         leds_active = false;
         counter = 0;
@@ -73,6 +87,9 @@ int64_t turn_off_callback(alarm_id_t id, void *user_data)
 
 int main()
 {
+    /**
+     * Realiza as inicializações necessárias
+     */
     stdio_init_all();
     init_leds();
     init_button();
@@ -81,6 +98,9 @@ int main()
 
         if (!gpio_get(BUTTON_A) && leds_active == false)
         {
+            /**
+             * Tratamento de Debounce
+             */
             sleep_ms(50);
 
             if (!gpio_get(BUTTON_A))
@@ -90,6 +110,9 @@ int main()
                 
                 leds_active = true;
 
+                /**
+                 * Chama a função de callback depois de 3 segundos para iniciar a mudança de estados dos leds
+                 */
                 add_alarm_in_ms(3000, turn_off_callback, NULL, false);
             }
         }
